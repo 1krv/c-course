a3e4736e1a9e6a768718e8e5ce956af0f9cfdf98
1krav <iegorkravchenko@proton.me>
2025-12-18 14:45:51 +0100

    Add implementation of prefixed-lenght strings.

---
+++ pls.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Initialize a prefixed lenght string with the specified
+ * string in 'init' of length 'len'.
+ *
+ * The created strings have the following layout:
+ *
+ * +-+-------------\\\
+ * |L|My string here
+ * +-+-------------\\\
+ *
+ * Where L is one unsigned byte states the total length of the string.
+ * Thus this strings are binary safe: zero bytes are permitted in the
+ * middle.
+ *
+ * Warning: this function does not check for buffer overflow. */
+char *ps_create(char *init, int len) {
+    char *s = malloc(1+len+1);
+    unsigned char *lenptr = (unsigned char*)s;
+    *lenptr = len;
+
+    for (int j = 0; j < len; j++) {
+        s[j+1] = init[j];
+    }
+
+    s[len+1] = 0;
+
+    return s;
+}
+
+/* Display the string 's' on the screen. */
+void ps_print(char *s) {
+    unsigned char *lenptr = (unsigned char*)s;
+    for (int j = 0; j < *lenptr; j++) {
+        putchar(s[j+1]);
+    }
+    printf("\n");
+}
+
+/* Return the pointer to the null terminated C string embedded
+ * inside our PS string 's'. */
+char *ps_getc(char *s) {
+    return s+1;
+}
+
+int main(void) {
+    char *mystr = ps_create("Hello World Hello World Hello World", 35);
+    ps_print(mystr);
+    printf("%s\n", ps_getc(mystr));
+    free(mystr);
+
+    return 0;
+}
