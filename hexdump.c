2340a8738ceb9b4cd78dc6425f714cf9c4288828
1krav <iegorkravchenko@proton.me>
2025-12-27 16:03:37 +0100

    Implement a hexdump-like function ic C

---
+++ hexdump.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define HEXDUMP_CHARS_PER_LINE 8
+
+struct pls {
+    long len;
+    char str[20];
+};
+
+void hexdump(void *p, size_t len) {
+    unsigned char *byte = p;
+    size_t po = 0;
+    for (size_t j = 0; j < len; j++) {
+        printf("%02X ", byte[j]);
+        if ((j + 1) % HEXDUMP_CHARS_PER_LINE == 0 || j == len - 1) {
+            if (j == len - 1) {
+                int pad = HEXDUMP_CHARS_PER_LINE - (len % HEXDUMP_CHARS_PER_LINE);
+                pad %= HEXDUMP_CHARS_PER_LINE;
+                for (int i = 0; i < pad; i++) printf("˜˜ ");
+            }
+
+            printf("\t");
+            for (size_t i = po; i <= j; i++) {
+                int c = isprint(byte[i]) ? byte[i] : '.';
+                printf("%c", c);
+            }
+            printf("\n");
+            po = j + 1;
+        }
+    }
+}
+
+int main (void) {
+    struct pls s;
+    s.len = 10;
+    // s.str = malloc(s.len+1);
+    memcpy(s.str, "1234567890", s.len+1);
+
+    hexdump(&s, sizeof(s));
+
+    return 0;
+}
