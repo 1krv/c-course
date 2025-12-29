37588e55f348d9e12e5337b5342e5c73f8c209f9
1krav <iegorkravchenko@proton.me>
2025-12-29 22:21:56 +0100

    File reading through libc and system calls

---
+++ typedef.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct {
+    int n;
+    int d;
+} fract;
+
+typedef fract *fractptr;
+
+int main(void) {
+    fract f;
+    fractptr fp = &f;
+    f.n = 10;
+    f.d = 20;
+
+    printf("%d/%d, my fraction is stored at %p\n", f.n, f.d, fp);
+
+    return 0;
+}
